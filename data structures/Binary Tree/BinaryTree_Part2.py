481103eaa23cf58177973a1a4fd9776a7a083a75
Sarthak-Shah <er.sarthak@outlook.com>
2025-02-17 23:03:35 -0500

    adding binary tree part 2

--- data structures/Binary Tree/BinaryTree_Part2.py
+++ data structures/Binary Tree/BinaryTree_Part2.py
@@ -1,13 +1,3 @@
-'''
-Diameter of tree = No. of nodes in the longest path between 2 leaves
-Cases:
-1. Tree diameter going through root node
-2. Not going through root node
-
-Height of node required here
-'''
-
-
 class TreeNode:
     def __init__(self, data):
         self.data = data
@@ -23,16 +13,15 @@ def height(rootNode: TreeNode):
     rh = height(rootNode.right)
     return max(lh, rh) + 1
 
-
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.right = TreeNode(3)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(5)
-root.right.left = TreeNode(6)
-root.right.right = TreeNode(7)
-
 # print(height(root.right))
+'''
+Diameter of tree = No. of nodes in the longest path between 2 leaves
+Cases:
+1. Tree diameter going through root node
+2. Not going through root node
+
+Height of node required here
+'''
 
 
 def diameter(root:TreeNode):
@@ -75,3 +64,48 @@ O(n2) time complexity since we are also calculating height along with diameter b
 #
 #
 # print(diamiter2(root))
+
+
+"""
+Given a binary tree, find its minimum depth.
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+Note: A leaf is a node with no children.
+
+Steps:
+1. If the tree is empty (root == None), return 0.
+2. If the node has only one child, we must explore the non-null subtree because the minimum depth requires reaching a leaf node.
+3. If both left and right children exist, recursively compute the minimum depth of both and return the smaller one + 1 (for the current node).
+"""
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def minDepth(root):
+    if not root:
+        return 0
+
+    # If no left subtree, return depth of right subtree + 1
+    if not root.left:
+        return minDepth(root.right) + 1
+
+    # If no right subtree, return depth of left subtree + 1
+    if not root.right:
+        return minDepth(root.left) + 1
+
+    # If both subtrees exist, return the minimum depth of both
+    return min(minDepth(root.left), minDepth(root.right)) + 1
+
+
+# Constructing the binary tree [3,9,20,null,null,15,7]
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+print(minDepth(root))  # Output: 2
