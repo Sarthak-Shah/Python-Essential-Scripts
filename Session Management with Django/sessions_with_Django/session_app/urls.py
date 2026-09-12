507b6a031907aba5c8712db230c42cec01e31139
Sarthak Shah <er.sarthak@outlook.com>
2026-09-11 20:39:11 -0400

    Delete Session Management with Django/sessions_with_Django directory
    Maintaining diff repo for Django

--- Session Management with Django/sessions_with_Django/session_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, anonymous_view, logged_in_view
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('anonymous', anonymous_view, name='anonymous_view'),
-    path('logged-in/', logged_in_view, name='logged-in'),
-]
